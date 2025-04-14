e50408e0dfc8b1e2c402dc2c914ad79b69a7e476
Yashpreet Singh <yashpreet1686@gmail.com>
2025-04-14 19:13:18 +0530

    added pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+//pages
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
