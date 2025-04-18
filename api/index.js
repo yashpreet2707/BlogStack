985f5dd1a2faf7581edf3f858116779dfdd6890b
Yashpreet Singh <yashpreet1686@gmail.com>
2025-04-18 12:33:27 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js"
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -26,6 +27,7 @@ app.listen(port, () => console.log(`Server is running at localhost:${port}`));
 app.get('/', (req,res)=> res.send("Hello buddy!")) // testing
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use('/api/post/', postRoutes);
 
 
 // middlewares
